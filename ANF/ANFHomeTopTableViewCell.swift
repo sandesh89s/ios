ee76aeb7599e4bb208df2c09ef06ace900f1b4b4
sandesh <sandesh@Sandeshs-MacBook-Pro-4.local>
2014-11-15 00:15:48 -0500

    first commit

---
+++ ANF/ANFHomeTopTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ANFHomeTopTableViewCell.swift
+//  ANF
+//
+//  Created by Sandesh Srinidhi on 11/11/14.
+//  Copyright (c) 2014 ANF. All rights reserved.
+//
+
+import UIKit
+
+class ANFHomeTopTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var imageLable: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
